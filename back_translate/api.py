642b4c2f9fa05e1d3242dfe52a1a384806546466
Christian Ritter <christian.h.ritter@gmail.com>
2019-11-09 09:51:18 -0500

    implementing fr - en with API

--- back_translate/api.py
+++ back_translate/api.py
@@ -1,18 +1,54 @@
+# Implementation of the FastAPI
+# Notes:
+#   * Could extend with upload capability: https://fastapi.tiangolo.com/tutorial/request-files/
+#
+
 from fastapi import FastAPI, Query
+import subprocess
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(
+@app.get("/fr_to_eng/")
+async def endpoint_fr_to_eng(
         q: str = Query(
-            None,
-            title="Query string xxxxxx",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
+            ..., # requires input, no default
+            title="French text to be translated",
+            description="Query string to translate from French to English.",
+            min_length=2, # min length string must be
+            max_length=1000 # max length string must be
         )
 ):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    #results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        results.update({"q": q})
-    return results
+        #results.update({"q": q})
+        translate_fr_to_english(q)
+    return q
+
+def translate_fr_to_english(french_text):
+    '''
+
+    :param french_text:
+    :return:
+    '''
+    with open('fr_text.txt','w') as f:
+        f.write(french_text)
+
+    # execute the translation
+
+    with open('run_translation_fr_eng.log', 'w') as f:
+        process = subprocess.Popen(['./run_translation_fr_eng.sh'],
+                                   stdout=f,
+                                   stderr=f)
+
+    #print(stdout, stderr)
+
+    try:
+        with open('back_trans_data/paraphrase/file_0_of_1.json') as f:
+            print('translation from file:')
+            print(f.readlines())
+    except:
+        print('translated file not available. Error!')
+
+
+
---
+++ back_translate/run_translation_fr_eng.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#'''
+#replicas: An argument for parallel preprocessing. For example, when replicas=3,
+#we divide the data into three parts, and only process one part
+#according to the worker_id.
+#'''
+replicas=1
+worker_id=0
+
+input_file='fr_text.txt'
+
+# Dirs
+data_dir=back_trans_data
+doc_len_dir=${data_dir}/doc_len
+forward_src_dir=${data_dir}/forward_src
+forward_gen_dir=${data_dir}/forward_gen
+para_dir=${data_dir}/paraphrase
+
+mkdir -p ${data_dir}
+mkdir -p ${forward_src_dir}
+mkdir -p ${forward_gen_dir}
+mkdir -p ${doc_len_dir}
+mkdir -p ${para_dir}
+
+echo "*** spliting paragraph ***"
+echo "write to "${para_dir}/file_${worker_id}_of_${replicas}.json
+
+# install nltk
+python split_paragraphs.py \
+  --input_file=${input_file} \
+  --output_file=${forward_src_dir}/file_${worker_id}_of_${replicas}.txt \
+  --doc_len_file=${doc_len_dir}/doc_len_${worker_id}_of_${replicas}.json \
+  --replicas=${replicas} \
+  --worker_id=${worker_id} \
+
+
+echo "*** forward translation ***"
+t2t-decoder \
+  --problem=translate_enfr_wmt32k \
+  --model=transformer \
+  --hparams_set=transformer_big \
+  --hparams="sampling_method=random,sampling_temp=${sampling_temp}" \
+  --decode_hparams="beam_size=1,batch_size=16" \
+  --checkpoint_path=checkpoints/enfr/model.ckpt-500000 \
+  --output_dir=/tmp/t2t \
+  --decode_from_file=${forward_src_dir}/file_${worker_id}_of_${replicas}.txt \
+  --decode_to_file=${forward_gen_dir}/file_${worker_id}_of_${replicas}.txt \
+  --data_dir=checkpoints
+
+echo "*** transform sentences back into paragraphs***"
+python sent_to_paragraph.py \
+  --input_file=${forward_gen_dir}/file_${worker_id}_of_${replicas}.txt \
+  --doc_len_file=${doc_len_dir}/doc_len_${worker_id}_of_${replicas}.json \
+  --output_file=${para_dir}/file_${worker_id}_of_${replicas}.json
+
+
